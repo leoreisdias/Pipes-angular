5ffd6c05e7157f865a84befddcb4a4c313f7d64a
leoreisdias <leoanrdord99@gmail.com>
2020-10-12 12:07:08 -0300

    Locale_ID

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -13,7 +13,7 @@ export class ExemplosPipesComponent implements OnInit {
     numeroPaginas: 288,
     preco: 36.00,
     data: new Date(2018, 9, 3),
-    url: "https://www.amazon.com.br/Queda-Gondolin-J-R-Tolkien/dp/8595083657/ref=asc_df_8595083657/?tag=googleshopp00-20&linkCode=df0&hvadid=379792561178&hvpos=&hvnetw=g&hvrand=15161706186504785299&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1031668&hvtargid=pla-786135656817&psc=1"
+    url: "link aqui"
   }
 
   constructor() { }
